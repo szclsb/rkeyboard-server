36be8178a7e04d7f54f9912c82a0d26dcbab9d26
Claudio <szclsb@gmail.com>
2021-12-12 18:14:55 +0100

    Impl simple socket.io server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('hello world');
+import express from 'express';
+import http from 'http';
+import { Server } from "socket.io";
+
+const port = 3000;
+const secret = 'mysecret';
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+io.on('connection', socket => {
+    console.log(`user connected: ${socket.id}`)
+
+    socket.on('key', key => {
+        process.stdout.write(key);
+    });
+    socket.on('disconnect', () => {
+        console.log(`user disconnected ${socket.id}`);
+    });
+});
+
+server.listen(port, () => {
+    console.log(`listening on *:${port}`);
+});
