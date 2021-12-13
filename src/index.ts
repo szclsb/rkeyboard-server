78fe935615410ab3d19b5c24862c9f6caf364e38
Claudio <szclsb@gmail.com>
2021-12-13 12:49:07 +0100

    Added broadcast

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import http from 'http';
-import { Server } from "socket.io";
+import {Server, Socket} from "socket.io";
 
 const port = 3000;
 const secret = 'mysecret';
@@ -8,13 +8,18 @@ const secret = 'mysecret';
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
+const users: { [id: string]: Socket; } = {}
+
 io.on('connection', socket => {
     console.log(`user connected: ${socket.id}`)
+    // users[socket.id] = socket;
 
     socket.on('key', key => {
-        process.stdout.write(key);
+        //todo difference between sender and subscriber
+        socket.broadcast.emit('key', key);
     });
     socket.on('disconnect', () => {
+        // delete users[socket.id];
         console.log(`user disconnected ${socket.id}`);
     });
 });
